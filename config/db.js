584544b7b8e10d560f25091f17ad7501b266d5c1
Theerat Punyasit <vetozer6417@gmail.com>
2024-03-26 23:43:28 +0700

    Mongoose connection changed

--- config/db.js
+++ config/db.js
@@ -1,12 +1,18 @@
 const mongoose = require('mongoose');
-require('dotenv').config({ path: 'config.env'});
+require('dotenv').config({ path: 'config.env' });
 
-const connectDB = async() => {
+const connectDB = async () => {
     mongoose.set('strictQuery', true);
-    const conn = await mongoose.connect(process.env.MONGO_URI,{
-    });
-
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true, // Add this line to ensure compatibility with newer versions of MongoDB
+            useUnifiedTopology: true // Add this line to ensure compatibility with newer versions of MongoDB
+        });
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error connecting to MongoDB: ${error.message}`);
+        process.exit(1); // Exit the process with a failure code
+    }
 }
 
 module.exports = connectDB;
